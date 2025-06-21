508e7b088d687892db45ec6098ac49cefc1fdad9
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-21 21:41:54 +0500

    Remove initial migration files and update Product model with additional fields; refactor Seller model structure.

--- base/models.py
+++ base/models.py
@@ -4,7 +4,19 @@ from django.db import models
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=255)
+    type = models.CharField(max_length=200)
+    quality = models.IntegerField()
+    weight = models.IntegerField()
+    image = models.ImageField(upload_to='cement-image/')
+    description = models.CharField(max_length=200)
+    origin = models.CharField(max_length=200)
+    cement_class = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -23,13 +35,4 @@ class Order(models.Model):
 
 
 class Seller(models.Model):
-    brand = models.CharField(max_length=255)
-    type = models.CharField(max_length=200)
-    quality = models.IntegerField()
-    weight_per_bag = models.IntegerField()
-    origin = models.CharField(max_length=200)
-    cement_class = models.CharField(max_length=20)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.brand
+    pass

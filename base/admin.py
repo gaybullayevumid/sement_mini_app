9634f7874ba4c4a0bd477352cbb9693c61ead8ea
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-21 21:45:12 +0500

    Add initial migration for Product and Order models; register models in admin

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Seller
+from .models import Seller, Product, Order
 
 # Register your models here.
 
 admin.site.register(Seller)
+admin.site.register(Product)
+admin.site.register(Order)

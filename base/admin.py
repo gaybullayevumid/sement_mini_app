a0de8d4749d3cf38dd78d64f61233521fc5bdfcd
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-21 21:52:14 +0500

    Add User model and register it in admin; create migration for User model

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Seller, Product, Order
+from .models import Seller, Product, Order, User
 
 # Register your models here.
 
 admin.site.register(Seller)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(User)
--- base/models.py
+++ base/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+ROLE_CHOICES = [
+    ("client", "Client"),
+    ("seller", "Seller"),
+]
+
+
+class User(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    full_name = models.CharField(max_length=200)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -9,7 +24,7 @@ class Product(models.Model):
     type = models.CharField(max_length=200)
     quality = models.IntegerField()
     weight = models.IntegerField()
-    image = models.ImageField(upload_to='cement-image/')
+    image = models.ImageField(upload_to="cement-image/")
     description = models.CharField(max_length=200)
     origin = models.CharField(max_length=200)
     cement_class = models.CharField(max_length=20)
@@ -31,7 +46,6 @@ class Order(models.Model):
 
     def __str__(self):
         return self.client
-    
 
 
 class Seller(models.Model):

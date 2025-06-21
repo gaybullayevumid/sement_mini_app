a9b7fa1b7b403c42529b2c7ba9d09eb0f6e27bb5
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-21 18:12:05 +0500

    Add Seller model and register in admin: Define fields for brand, type, quality, weight_per_bag, origin, and cement_class

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Seller(models.Model):
+    brand = models.CharField(max_length=255)
+    type = models.CharField(max_length=200)
+    quality = models.IntegerField()
+    weight_per_bag = models.IntegerField()
+    origin = models.CharField(max_length=200)
+    cement_class = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.brand

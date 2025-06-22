73700be905c39bf48770ed6b96feb80c6eb08aec
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-22 12:48:13 +0500

    Refactor code structure for improved readability and maintainability

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from .models import Product, Order
 
 class SementMarketView(TemplateView):
-    template_name = "sement_market.html"
+    template_name = "pages/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

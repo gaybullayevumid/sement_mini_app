5539cbcfba500796690a233ffa53456476170b03
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-22 23:56:10 +0500

    Refactor code structure for improved readability and maintainability

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "6667385868:AAEgEGKSM_YoHyGBAd2Xf4JwBt8tRwen6U8"
-WEBAPP_URL = "https://fb63-2a05-45c2-6067-de00-9d64-e4ba-fcd2-65f1.ngrok-free.app"
+WEBAPP_URL = "https://dede-2a05-45c2-6067-de00-a89a-272a-7ab1-d743.ngrok-free.app"

a93e567f004e0db432776723ce68ab9f9aba08a8
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-23 09:46:53 +0500

    Update WEBAPP_URL to new ngrok address

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "6667385868:AAEgEGKSM_YoHyGBAd2Xf4JwBt8tRwen6U8"
-WEBAPP_URL = "https://e319-2a05-45c2-6067-de00-a89a-272a-7ab1-d743.ngrok-free.app"
+WEBAPP_URL = "https://8071-89-236-217-6.ngrok-free.app"

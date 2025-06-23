18f3fe6171b6796f21dabef78fd62b7d6bb5b130
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-23 17:12:01 +0500

    Add login path to urlpatterns for authentication

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("api/", include("base.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema json'),
 ] 

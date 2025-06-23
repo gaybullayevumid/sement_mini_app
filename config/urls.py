44b2df9fb9bbea1ac61c65063b2a19923756115a
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-23 17:03:03 +0500

    Add newline at the end of urls.py for consistency

--- config/urls.py
+++ config/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema json'),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+

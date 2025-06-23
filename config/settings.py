aeeb21784866d3f9e7fccbcb76f562584b66bc3d
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-23 15:40:41 +0500

    Update requirements and add render configuration for web service

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,7 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# settings.py oxiriga
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"

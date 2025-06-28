5dfe6fb31925b6fbf8d3b3830359b0b699251adf
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-28 15:53:04 +0500

    Refactor: Update database configuration to use dj_database_url for PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgres://umidch1k:Iy0E420KycH00V5qyyNjNWiZ1bDVsKHm:5432/sement_savdo'
+    )
 }
 
 

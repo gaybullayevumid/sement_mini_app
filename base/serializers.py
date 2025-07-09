bcf57e6ab2ce128e5ab50d31ae724674de21d45e
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-09 11:06:34 +0500

    feat: add category and brand models

--- base/serializers.py
+++ base/serializers.py
@@ -1,16 +1,26 @@
 from rest_framework import serializers
-from .models import CustomUser, Product, Order, Cart
+from .models import CustomUser, Product, Order, Cart, Brand, Category
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'first_name', 'last_name', 'phone', 'user_type']
 
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name', 'description']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+
 class ProductSerializer(serializers.ModelSerializer):
     seller = UserSerializer(read_only=True)
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'seller']
+        fields = ['id', 'name', 'description', 'price', 'category', 'image', 'quantity', 'brand', 'seller', 'created_at']
 
 class OrderSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
--- base/views.py
+++ base/views.py
@@ -1,26 +1,57 @@
 from rest_framework import viewsets, permissions
-from .models import CustomUser, Product, Order, Cart
-from .serializers import UserSerializer, ProductSerializer, OrderSerializer, CartSerializer
+from .models import CustomUser, Product, Order, Cart, Brand, Category
+from .serializers import (
+    UserSerializer,
+    ProductSerializer,
+    OrderSerializer,
+    CartSerializer,
+    CategorySerializer,
+    BrandSerializer,
+)
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=["get"])
     def sellers(self, request):
-        sellers = CustomUser.objects.filter(user_type='seller')
+        sellers = CustomUser.objects.filter(user_type="seller")
         serializer = self.get_serializer(sellers, many=True)
         return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=False, methods=["get"])
+    def products(self, request):
+        category_id = request.query_params.get("category_id")
+        if category_id:
+            products = Product.objects.filter(category_id=category_id)
+        else:
+            products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()

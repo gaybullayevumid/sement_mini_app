9d8905e6a205d26ec49ede05bc12d7b98d616d42
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-10 13:33:18 +0500

    feat: cors settings

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://sementsavdo.uz",
-    "http://localhost:3000",
+    "http://localhost:5175",
 ]
 
 ROOT_URLCONF = 'config.urls'

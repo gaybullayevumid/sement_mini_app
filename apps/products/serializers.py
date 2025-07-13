fae60f52757d0aeb0fe8e5d40558f1ac87a367d4
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-14 01:52:36 +0500

    feat(products): add serializers

---
+++ apps/products/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import CustomUser, Product, Order, Cart, Brand, Category
+from base.serializers import SellerShortSerializer
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    seller = SellerShortSerializer(read_only=True)
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'image', 'quantity', 'brand', 'seller', 'created_at']
+
+class SellerWithProductsSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'first_name', 'last_name', 'phone', 'user_type', 'products']
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name', 'description']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+
+class OrderSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'product', 'quantity', 'created_at']
+
+class CartSerializer(serializers.ModelSerializer):
+    orders = OrderSerializer(many=True, read_only=True)
+    class Meta:
+        model = Cart
+        fields = ['id', 'client', 'orders']

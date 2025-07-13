2e9f72b2a4e8e58d16baa19bb0ba88d2bb40d1b6
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-14 01:54:08 +0500

    feat(products): create views and urls

---
+++ apps/products/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    ProductViewSet,
+    CartViewSet,
+    OrderViewSet,
+    BrandViewSet,
+    CategoryViewSet,
+)
+
+router = DefaultRouter()
+router.register(r"products", ProductViewSet)
+router.register(r"carts", CartViewSet)
+router.register(r"orders", OrderViewSet)
+router.register(r"brands", BrandViewSet)
+router.register(r"categories", CategoryViewSet, basename="category")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser, Product, Order, Cart, Brand, Category
+from .serializers import (
+    ProductSerializer,
+    OrderSerializer,
+    CartSerializer,
+    CategorySerializer,
+    BrandSerializer,
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=False, methods=["get"])
+    def products(self, request):
+        category_id = request.query_params.get("category_id")
+        if category_id:
+            products = Product.objects.filter(category_id=category_id)
+        else:
+            products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    @action(detail=False, methods=["get"], permission_classes=[permissions.IsAuthenticated])
+    def my_products(self, request):
+        """
+        Foydalanuvchi (seller) o'zining barcha productlarini ko'radi
+        """
+        products = Product.objects.filter(seller=request.user)
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer

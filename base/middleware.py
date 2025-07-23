567de0af994db703afa78c84b154410165f9cb0d
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-23 22:50:27 +0500

    feat: remove base app

--- base/middleware.py
+++
@@ -1,9 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class CustomCorsMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
-        response["Access-Control-Allow-Headers"] = "Origin, Content-Type, Accept, Authorization"
-        response["Access-Control-Allow-Credentials"] = "true"
-        return response
